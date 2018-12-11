70676d0248b5b54feec6301ad310d952b6cfd71d
alex <arader@comcast.net>
2018-12-10 21:47:36 -0500

    I think thats the master server.

--- NetworkingPlugin/StructuresLibrary/inludes/Structures.h
+++ NetworkingPlugin/StructuresLibrary/inludes/Structures.h
@@ -10,23 +10,65 @@
 #include "RakNet/BitStream.h" // include changed to correct directory
 #include "RakNet/RakNetTypes.h"  // MessageID // include changed to correct directory
 #include <iostream>
+#include <Vector>
 
+#include <connection.hpp>
+#include <packet.h>
+
+//MessageTypes for the master server
+enum GameMessages
+{
+	ID_GAME_MESSAGE_ClientConnecting = PACKET_END, // for a client connecting
+	ID_GAME_MESSAGE_ServerConnecting, // for a server connecting
+	ID_GAME_MESSAGE_MessageSending,   // client sending a message to people / person
+	ID_GAME_MESSAGE_ClientDisconnect, // client disconnecting from game
+	ID_GAME_MESSAGE_ServerDisconnect, // server disconnecting/ closing to master
+	ID_GAME_MESSAGE_ClientServerConn, // client requesting a connection to the server
+	ID_GAME_MESSAGE_ClientServerCan,  // client could connect
+	ID_GAME_MESSAGE_ClientServerCant, // client cant connect
+};
+
+//Client structure and call location used primarilly for messaging 
 struct Client
 {
-	char mName[32];
-	RakNet::RakNetGUID mClientsGuid;
+	char typeID = ID_GAME_MESSAGE_ClientConnecting;
+	char mName[NAME_SIZE];
+	Connection mClientsGuid;
 };
 
+//keeps a server's info for use in finding a 
 struct Server
 {
-	char mName[32];
-	char mIpAddress[14];
-	int mMaxClients, mCurrentClientNumber;
+	char typeID = ID_GAME_MESSAGE_ServerConnecting;
+	char mName[NAME_SIZE];
+	char mSysAddress[NAME_SIZE];
+	//char mIpAddress[14];
+	int mCurrentClientNum, mMaxConnections;
+
+	Connection mAddress;
+
+	unsigned int mPort;
 };
 
+//super simple message storing and sending
 struct ChatMessage
 {
+	char typeID = ID_GAME_MESSAGE_MessageSending;
 	char mMessage[256];
+};
+
+struct ServerLocation
+{
+	char typeID = ID_GAME_MESSAGE_ClientServerConn;
+	Connection mAddress;
+};
+
+struct ConnectionPossible
+{
+	char typeID = ID_GAME_MESSAGE_ClientServerCan;
+
+	char mAddress[NAME_SIZE];
+	unsigned int mPort;
 };
 
 
